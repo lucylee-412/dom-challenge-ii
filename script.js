889644d568e9885ea1b0157030cd97c98fb21324
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 00:34:09 -0500

    Add function to send alert to window on mouseover

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const argument = document.getElementById("who-is-right")
 function whoIsRight(me) {
     argument.innerText = me;
 }
+
+function nover() {
+    alert("Hey, I told you not to hover over me! >:O");
+}
